81975412c3b503306dcfcd87c798712c3ad967d6
Raotrux <56573919+Raotrux@users.noreply.github.com>
2020-05-09 17:06:36 -0700

    Setting Up NavBar component
    Added logo.png and logo.svg in \components\ and \src\ to reduce possible error messages. Will remove unneeded files as progress is made.
    
    Updated App.js file to call NavBar component and created a NavBar component.
    
    Still need to adjust logo size and positioning of text/buttons.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import NavBar from "./components/navbar.component";
+
 import LandingPage from "./components/landing-page.component";
 import CreateFlashcardSet from "./components/create-flashcardset.component";
 import EditFlashcardSet from "./components/edit-flashcardset.component";
@@ -10,37 +12,13 @@ import FlashcardSetList from "./components/flashcardset-list.component";
 import SignIn from "./components/signup.component";
 import SignUp from "./components/signup.component";
 
-import dex_Icon from "./dexIcon.png";
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/" target="_blank">
-              <img src={dex_Icon} width="170" height="105" alt="" />
-            </a>
-              <div className="collpase navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                  <li className="navbar-item">
-                    <Link to="/search" className="nav-link">Search</Link>
-                  </li>
-                  <li className="navbar-item">
-                    <Link to="/list" className="nav-link">Browse</Link>
-                  </li>
-                  <li className="navbar-item">
-                    <Link to="/create" className="nav-link">Create</Link>
-                  </li>
-                  <li className="navbar-item">
-                    <Link to="/signup" className="nav-link">Sign Up</Link>
-                  </li>
-                  <li className="navbar-item">
-                    <Link to="/signin" className="nav-link">Sign In</Link>
-                  </li>
-                </ul>
-              </div>
-          </nav>
+        <NavBar></NavBar>
           <br/>
           <Route path="" exact component={LandingPage} />
           <Route path="/list" exact component={FlashcardSetList} />
@@ -48,7 +26,6 @@ class App extends Component {
           <Route path="/create" component={CreateFlashcardSet} />
           <Route path="/signup" component={SignUp} />
           <Route path="/signin" component={SignIn} />
-        </div>
       </Router>
     );
   }
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -2,44 +2,39 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-export default class LandingPage extends Component {
+//import logo from './logo.png';
+import { ReactComponent as Logo } from './logo.svg';
+
+export default class Navbar extends Component {
     render (){
         return (
-            <Router>
-                <div className="container">
+            <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="/" target="_blank">
-                    <img src={dex_Icon} width="170" height="105" alt="" />
+                    <a class="navbar-brand" target="_blank">
+                    <Logo width="170" height="105" alt="" />
                     </a>
+                    <Link to="/" className="navbar-brand">Dex</Link>
                     <div className="collpase navbar-collapse">
                         <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
+                            <li className="navbar-item">
                             <Link to="/search" className="nav-link">Search</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/list" className="nav-link">Browse</Link>
-                        </li>
-                        <li className="navbar-item">
+                            </li>
+                            <li className="navbar-item">
+                            <Link to="/" className="nav-link">Browse</Link>
+                            </li>
+                            <li className="navbar-item">
                             <Link to="/create" className="nav-link">Create</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/signup" className="nav-link">Sign Up</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/signin" className="nav-link">Sign In</Link>
-                        </li>
+                            </li>
+                            <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Sign Up</Link>
+                            </li>
+                            <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Sign In</Link>
+                            </li>
                         </ul>
                     </div>
                 </nav>
-                <br/>
-                <Route path="" exact component={LandingPage} />
-                <Route path="/list" exact component={FlashcardSetList} />
-                <Route path="/edit/:id" component={EditFlashcardSet} />
-                <Route path="/create" component={CreateFlashcardSet} />
-                <Route path="/signup" component={SignUp} />
-                <Route path="/signin" component={SignIn} />
-                </div>
-            </Router>
+            </div>
         )
     }
 }
