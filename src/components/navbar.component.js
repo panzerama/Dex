d0d4fe9ea4959783e113203867a054273b7ee727
Raotrux <56573919+Raotrux@users.noreply.github.com>
2020-05-11 00:02:59 -0700

    Worked on navbar stuff some more
    Having trouble with getting the search button to display inline.

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -8,43 +8,48 @@ import { ReactComponent as Logo } from './img/dexOwl.svg';
 export default class Navbar extends Component {
     render (){
         return (
-                <nav className="navbar">
-                        <div className="navbar-logo"> 
+                <nav className="navbar-container">
+                        <div className="navbar-logo-container"> 
                                 <Logo />
                         </div>
-                    <div className="navbar-list">  
+                    <div className="navbar-list-container">  
                         <Link to="/" className="nav-link">
-                            <button type="button" className="button">  
+                            <button type="button" className="navbar-button active">  
                                 Home
                             </button>
                         </Link> 
-                        <Link to="/search" className="nav-link" >
-                            <button type="button" className="button">  
-                                Search
-                            </button>
-                        </Link>
+
                         <Link to="/browse" className="nav-link">
-                            <button type="button" className="button">
+                            <button type="button" className="navbar-button">
                                 Browse
                             </button>
                         </Link>
                         <Link to="/create" className="nav-link">
-                            <button type="button" className="button">
+                            <button type="button" className="navbar-button">
                                 Create
                             </button>
                         </Link>
                     </div>
-                    <div className="navbar-signin">  
+                    <div className="navbar-search-container">
+                        <form action="/action_page.php">
+                            <input type="text" placeholder="Search.." name="search"></input>
+                            <button type="button" className="navbar-button">  
+                                Search
+                            </button>
+                        </form>       
+                    </div>
+                    <div className="navbar-user-container">
                         <Link to="/signup" className="nav-link">
-                            <button type="button" className="button">
+                            <button type="button" className="navbar-button">
                                 Sign Up
                             </button>
                         </Link>
                         <Link to="/signin" className="nav-link">
-                            <button type="button" className="button">
+                            <button type="button" className="navbar-button">
                                 Sign In
                             </button>   
                         </Link> 
+                        
                     </div>
                 </nav>
         )
