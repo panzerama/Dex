edcf8d0bb57c5804b433611bd965da513e48e3f0
Raotrux <56573919+Raotrux@users.noreply.github.com>
2020-05-11 20:33:44 -0700

    New navbar prototype
    Started the code from scratch and tried to implement more generalized class naming.

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -3,55 +3,49 @@ import { Link } from 'react-router-dom';
 import "../App.css";
 
 //import logo from './logo.png';
-import { ReactComponent as Logo } from './img/dexOwl.svg';
+import { ReactComponent as Logo } from './img/dexOwl.svg'; 
 
 export default class Navbar extends Component {
     render (){
-        return (
-                <nav className="navbar-container">
-                        <div className="navbar-logo-container"> 
-                                <Logo />
-                        </div>
-                    <div className="navbar-list-container">  
-                        <Link to="/" className="nav-link">
-                            <button type="button" className="navbar-button active">  
-                                Home
-                            </button>
-                        </Link> 
+                    return (
+            <nav class="navbar">
+            <a class="navbar-brand" href="#"><Logo /></a>
 
-                        <Link to="/browse" className="nav-link">
-                            <button type="button" className="navbar-button">
-                                Browse
-                            </button>
-                        </Link>
-                        <Link to="/create" className="nav-link">
-                            <button type="button" className="navbar-button">
-                                Create
-                            </button>
-                        </Link>
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="navbar-form">
+                    <input class="navbar-text" placeholder="Search" />
+                    <button class="navbar-submit" >Search</button>
+                </form>
+            </nav>
+
+            /* Dropdown Code 
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
                     </div>
-                    <div className="navbar-search-container">
-                        <form action="/action_page.php">
-                            <input type="text" placeholder="Search.." name="search"></input>
-                            <button type="button" className="navbar-button">  
-                                Search
-                            </button>
-                        </form>       
-                    </div>
-                    <div className="navbar-user-container">
-                        <Link to="/signup" className="nav-link">
-                            <button type="button" className="navbar-button">
-                                Sign Up
-                            </button>
-                        </Link>
-                        <Link to="/signin" className="nav-link">
-                            <button type="button" className="navbar-button">
-                                Sign In
-                            </button>   
-                        </Link> 
-                        
-                    </div>
-                </nav>
+                </li>
+            */
         )
     }
 }
